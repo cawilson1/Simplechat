5d7c3511704c090765d20d5c1fd7f254857f7c25
Zach <zachary.bedell@gmail.com>
2018-02-11 22:33:33 +0000

    adds changes to server side

--- network_files/newtcpserver.c
+++ network_files/newtcpserver.c
@@ -12,7 +12,14 @@
 
 int main(int argc, char* argv[])
 {
-	
+	struct packet{
+			short type;
+			char* uName;//manually check that this and two below are no greater than MAX_LINE in size
+			char* mName;
+			char* data;
+		};
+
+		struct packet packet_reg;
 
 	struct sockaddr_in sin;
 	struct sockaddr_in clientAddr;
@@ -21,13 +28,14 @@ int main(int argc, char* argv[])
 	int len;
 
 
-	
+
 
 	/* setup passive open */
 	if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0){
 		perror("tcpserver: socket");
 		exit(1);
 	}
+
 
 
 	/* build address data structure */
@@ -43,15 +51,50 @@ int main(int argc, char* argv[])
 	}
 	listen(s, MAX_PENDING);
 
+
 	/* wait for connection, then receive and print text */
+	/* initalizes len for use in the accept() function */
+	len = sizeof(struct sockaddr_in);
 	while(1){
 		if((new_s = accept(s, (struct sockaddr *)&clientAddr, &len)) < 0){
 			perror("tcpserver: accept");
 			exit(1);
 		}
+		/* Prints request sent to server */
+		printf("\n Accepted new client request, socket id is %d \n", new_s);
 
-		printf("\n Client's port is %d \n", ntohs(clientAddr.sin_port)); 
 
+		if(recv(new_s, &packet_reg, sizeof(packet_reg), 0) < 0){
+			printf("\n Could not receive first registration packer\n");
+			exit(1);
+		}
+		struct registrationTable{
+			int port;
+			int sockid;
+			char mName[MAX_LINE];
+			char uName[MAX_LINE];
+		};
+		struct registrationTable table[10];
+
+		table[0].port = clientAddr.sin_port;
+		table[0].sockid = new_s;
+		strcpy(table[0].uName, packet_reg.uName);
+		strcpy(table[0].mName, packet_reg.mName);
+
+		printf("\n Client's port is %d \n", ntohs(clientAddr.sin_port));
+
+		/* Sends accept confirmation to client */
+		/* I'm assuming 'sin' is the confirmation packet containing
+		information about the server address, and I'm setting the type to 221 */
+		sin.type = htons(221);
+		if(send(new_s,(struct sockaddr *)&sin, sizeof(sin)) < 0){
+			perror("tcpserver: send");
+			exit(1);
+		}
+		/*prints response sent by server */
+		printf("\n Server sent confirmation \n");
+
+		/*prints prints chat message from client */
 		while(len = recv(new_s, buf, sizeof(buf), 0))
 			fputs(buf, stdout);
 		close(new_s);
