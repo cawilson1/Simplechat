36c88e068327f97e085c31762f9e781bb68567ae
Casey Wilson <cawilson1@g.cofc.edu>
2018-02-11 18:20:19 -0500

    client sends 121 type to server and server can receive the packet and print 121

--- network_files/newtcpclient.c
+++ network_files/newtcpclient.c
@@ -5,34 +5,66 @@
 #include<netdb.h>
 #include<string.h>
 #include<stdlib.h>
+#include<sys/param.h>
 
-#define SERVER_PORT 5432
+#define SERVER_PORT 8017
 #define MAX_LINE 256
+
+struct packet{
+	short type;
+	char* uname;//manually check that this and two below are no greater than MAX_LINE in size
+	char* mname;
+	char* data;
+};	
+
 
 int main(int argc, char* argv[])
 {
-	
+
+	struct packet packet_reg;
+
 	struct hostent *hp;
 	struct sockaddr_in sin;
 	char *host;
+	char myMachineName[MAXHOSTNAMELEN];
 	char buf[MAX_LINE];
 	int s;
 	int len;
 
-	if(argc == 2){
+	fprintf(stdout,"argc is %d\n", argc); 
+	if(argc == 3){
 		host = argv[1];
+		packet_reg.uname=argv[2];//strcpy causes segmentation fault
 	}
 	else{
-		fprintf(stderr, "usage:newclient server\n");
+		fprintf(stderr, "incorrect argument count\n");
 		exit(1);
 	}
 
+
 	/* translate host name into peer's IP address */
-	hp = gethostbyname(host);
+	hp = gethostbyname(host);//old way
 	if(!hp){
 		fprintf(stderr, "unkown host: %s\n", host);
 		exit(1);
 	}
+	
+	//hostname for current client machine instance
+	gethostname(myMachineName, sizeof myMachineName);
+	printf("me: %s\n", myMachineName);
+
+	//create registration packet from command line args
+	//packet type
+	packet_reg.type = htons(121);
+	//set packet machine name
+	strcpy(packet_reg.mname, myMachineName);	
+	//packet_reg.mname = myhostname;
+
+	printf("Username: ");
+	printf(packet_reg.uname);
+	printf("\nINFO: \n");
+	printf("%u\n", ntohs(packet_reg.type));
+	printf(packet_reg.mname);
 
 	/* active open */
 	if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0){
@@ -48,14 +80,25 @@ int main(int argc, char* argv[])
 
 	
 	if(connect(s,(struct sockaddr *)&sin, sizeof(sin)) < 0){
-		perror("tcpclient: connect");
+		perror("tcpclient: connectasdfasdf");
 		close(s);
 		exit(1);
 	}
 	/* main loop: get and send lines of text */
 	while(fgets(buf, sizeof(buf), stdin)){
-		buf[MAX_LINE-1] = '\0';
-		len = strlen(buf) + 1;
-		send(s, buf, len, 0);
+		//send the registration packet to the server
+		if(send(s, &packet_reg, sizeof(packet_reg), 0) < 0)
+		{
+			printf("\nSend failed\n");
+			exit(1);
+		}
+		else{
+			printf("%u", ntohs(packet_reg.type));	
+		}	
+	//	buf[MAX_LINE-1] = '\0';
+	//	len = strlen(buf) + 1;
+	//	send(s, buf, len, 0);
 	}
+
+
 }
--- network_files/newtcpserver.c
+++ network_files/newtcpserver.c
@@ -6,13 +6,22 @@
 #include<string.h>
 #include<stdlib.h>
 
-#define SERVER_PORT 5432
+#define SERVER_PORT 8017
 #define MAX_LINE 256
 #define MAX_PENDING 5
+
+struct packet{
+	short type;
+	char uname[MAX_LINE];//manually check that this and two below are no greater than MAX_LINE in size
+	char* mname;
+	char* data;
+};	
+
 
 int main(int argc, char* argv[])
 {
 	
+	struct packet packet_reg;	
 
 	struct sockaddr_in sin;
 	struct sockaddr_in clientAddr;
@@ -49,12 +58,35 @@ int main(int argc, char* argv[])
 			perror("tcpserver: accept");
 			exit(1);
 		}
+		
+		printf("\nClient's port is %d \n", ntohs(clientAddr.sin_port)); 
 
-		printf("\n Client's port is %d \n", ntohs(clientAddr.sin_port)); 
+		//error
+		if(recv(new_s, &packet_reg, sizeof(packet_reg), 0) < 0){
+			printf("\nCould not receive the first registration packet\n");
+			exit(1);
+		}
+		//Registration packet
+		else if(htons(packet_reg.type)==121){
+			printf("\nRegistration packet:");
+			printf("\nPacket type: ");
+			printf("%u",htons(packet_reg.type));
+			
+			//Don't know why this isn't working.
+			printf("\nUsername: ");
+			printf("%c",packet_reg.uname);
+			
+		}
 
 		while(len = recv(new_s, buf, sizeof(buf), 0))
 			fputs(buf, stdout);
 		close(new_s);
+
+
+
+
+		//printf("Packet received. Username :");
+		//printf(packet_reg.uname);
 
 	}
 }
